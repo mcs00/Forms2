1ce6f35c17c3e87a448105b2fad61a64a25c84b2
mcs00 <mcs00@iesemilidarder>
2019-10-17 01:17:00 +0200

    m

---
+++ js/game.js
@@ -0,0 +1,17 @@
+
+function pillarDatos() {
+    let nombre = document.getElementById("nombre");
+    let lanzamiento = document.getElementById("lanzamiento");
+    let pegi = document.getElementById("pegi");
+    let desarrollador = document.getElementById("desarrollador");
+    let genero = document.getElementById("genero");
+
+    document.getElementById("salidaNombre").innerHTML = nombre;
+    document.getElementById("lanzamiento").innerHTML = lanzamiento;
+    document.getElementById("pegi").innerHTML = pegi;
+    document.getElementById("genere").innerHTML = genere;
+}
+
+
+
+
